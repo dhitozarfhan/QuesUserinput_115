b3a7bc1ba0394ae5b4a673912fb6e556c4ca29f4
dhitozarfhan <dhitozarfhan@gmail.com>
2025-10-30 14:58:23 +0700

    commit pertama

---
+++ app/src/main/java/com/example/prakpam4/MainActivity.kt
@@ -0,0 +1,47 @@
+package com.example.prakpam4
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import com.example.prakpam4.ui.theme.Prakpam4Theme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContent {
+            Prakpam4Theme {
+                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
+                    Greeting(
+                        name = "Android",
+                        modifier = Modifier.padding(innerPadding)
+                    )
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun Greeting(name: String, modifier: Modifier = Modifier) {
+    Text(
+        text = "Hello $name!",
+        modifier = modifier
+    )
+}
+
+@Preview(showBackground = true)
+@Composable
+fun GreetingPreview() {
+    Prakpam4Theme {
+        Greeting("Android")
+    }
+}
---
+++ app/src/main/java/com/example/prakpam4/ui/theme/TeksEdit.kt
@@ -0,0 +1,2 @@
+package com.example.prakpam4.ui.theme
+
